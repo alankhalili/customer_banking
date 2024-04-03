8b5af92052d758a8e6ebe3a2c75b77744a66d87c
alankhalili <67973298+alankhalili@users.noreply.github.com>
2024-04-03 00:03:54 -0400

    Update files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# customer_banking
+## Customer_banking
+Design a banking application that will compute the earned interest and ending balance of a savings and CD account
+
+## Introduction:
+
+
+Functionality include:
+    - Customer enters financial datat
+    - The scrips are modular via import feature
+    - Print ending balance and interest earned
+
+## Installation:
+Requires Python 10.
+
+## Usage:
+n/a
+
+## Configuration:
+The program is modular with four components
+    - Account - to track the account balance
+    - CD - to perform the CD calculations and update Account
+    - Savings - to perform the savings calculations in update the Account
+    - Customer - This where the customer interfaces with the system.  It is also where the other modules suppor the programs objective.
+
+## License:
+None required
+
+## Acknowledgments:
+Thank Eric for support.
+
+## Additional Sections:
+N/A
+
+## Footer:
+N/A
--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,13 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+#Import dependencies
+import sys
+#Import savings functionality
+from savings_account import create_savings_account
+#Import CD funtionality
+from cd_account import create_cd_account
+#Inport account information
+from Account import Account
 
 # Define the main function
 def main():
@@ -9,21 +17,29 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_deposit = int(input('\nInput your opening savings deposit. '))
+    savings_interest = float(input('Input your desired savings interest rate as a whole number. '))
+    savings_months = int(input('How many months do you want to earn interest on your savings accountr? '))
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
-
+    end_bal, interest_earned = create_savings_account(savings_deposit, savings_interest, savings_months)
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
-    # Prompt the user to set the CD balance, interest rate, and months for the CD account.
+    
+    print(f'\nThe interest earned for the ${savings_deposit: ,.2f} savings account is ${interest_earned: ,.2f}')
+    print(f'At the end of the {savings_months} months period, the savings account balance will be ${end_bal: ,.2f}')
+          # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance = int(input('\nInput your opening CD deposit. '))
+    cd_interest = float(input('Input your desired CD interest rate. '))
+    cd_maturity = int(input('How many months do you want to earn interest on your CD?  '))
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    end_bal_cd, interest_earned_cd = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f'\nThe interest earned for the {cd_maturity} months CD will be ${interest_earned_cd: ,.2f}')
+    print(f'At the end of the maturity period, the CD balalnce will increase by the ${interest_earned_cd: ,.2f} of interest to ${end_bal_cd: ,.2f}\n')
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
